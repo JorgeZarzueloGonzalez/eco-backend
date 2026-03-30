164731e3ef29c678fef566ea202f7d501c4d2544
Jorge Zaruzelo <jorgezargon@gmail.com>
2026-03-30 23:27:45 +0200

    refactor: Remove unused import for Value annotation in SongService

--- src/main/java/es/jorgezarzuelo/eco_backend/service/SongService.java
+++ src/main/java/es/jorgezarzuelo/eco_backend/service/SongService.java
@@ -2,7 +2,6 @@ package es.jorgezarzuelo.eco_backend.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import es.jorgezarzuelo.eco_backend.dto.album.AlbumSummaryDto;
